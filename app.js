0bccd801830640edbf8d0708cdfe7853cb3060b3
ZickFz <zickfz@gmail.com>
2017-04-26 01:47:35 +0700

    25042017

--- app.js
+++ app.js
@@ -1,2 +1,9 @@
 var g = G$('John', 'Doe');
-console.log(g);
+
+g.greet().setLang('es').greet(true).log();
+
+$('#login').click(function(){
+    var loginGrtr = G$('John', 'Doe');
+    $('#logindiv').hide();
+    loginGrtr.setLang($('#lang').val()).HTMLGreeting('#greeting', true).log();
+});
